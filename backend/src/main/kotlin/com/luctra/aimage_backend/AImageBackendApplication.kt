1ccf2f32d80bcee2d925c805c7207a451c197284
Lucas Tran <lucastran1107@gmail.com>
2025-06-23 21:22:08 +0200

    feat(backend): :tada: generate gradle kotlin springboot applicationin backend

---
+++ backend/src/main/kotlin/com/luctra/aimage_backend/AImageBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.luctra.aimage_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AImageBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<AImageBackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/luctra/aimage_backend/AImageBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.luctra.aimage_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AImageBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
