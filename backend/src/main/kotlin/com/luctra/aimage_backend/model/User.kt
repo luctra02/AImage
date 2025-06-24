7d1c07ed59fe7e9e5b12c37cc0194d163874fab9
Lucas Tran <lucastran1107@gmail.com>
2025-06-25 01:28:20 +0200

    feat(backend): :card_file_box: create model for user in backend

---
+++ backend/src/main/kotlin/com/luctra/aimage_backend/model/User.kt
@@ -0,0 +1,19 @@
+package com.luctra.aimage_backend.model
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(unique = true, nullable = false)
+    val email: String,
+
+    @Column(nullable = false)
+    val password: String,
+
+    val name: String? = null
+)
